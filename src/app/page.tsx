50aeed9a8090b97db051a8a0fc652c1682767c8e
AmitPradhan12 <amit.p@iosys.in>
2024-08-13 16:11:48 +0530

    du distanciel page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import Expertsinsocialintervention from "./componet/Expertsinsocialintervention"
 import NotreProcessus from "./componet/NotreProcessus";
 import Questions from "./componet/Questions";
 import Footer from "./componet/Footer";
+import Choisir from "./componet/Choisir";
 
 export default function Home() {
   return (
     <div>
       <Expertsinsocialintervention />
+      <Choisir/>
       <NotreProcessus />
       <Questions />
       <Footer/>
