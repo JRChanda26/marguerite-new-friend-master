8fc2d91a92b4527f99a5a99c31cfa0bb45f36f5d
JRChanda26 <jyotiranjan.c@iosys.in>
2024-08-14 18:06:12 +0530

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,14 @@ import Choisir from "./componet/Choisir";
 import NosActes from "./componet/NosActes";
 import Liberez from "./componet/Liberez";
 import Motsdenotreclient from "./componet/Motsdenotreclient";
+import SoinChez from "./componet/SoinChez";
 
 
 export default function Home() {
   return (
     <div>
       <Expertsinsocialintervention />
+      <SoinChez/>
       <Choisir/>
       <NotreProcessus />
       <Motsdenotreclient/>
