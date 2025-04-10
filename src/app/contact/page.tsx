d66898e820102ed03dad48c87a64f02e40d6994f
JRChanda26 <jyotiranjan.c@iosys.in>
2025-04-10 19:17:32 +0530

    done

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1425,7 +1425,7 @@ export default function Contact() {
                     background: "#292F36",
                     boxShadow: "none",
                   },
-                  margin: isMax || isMax5 ? "-200px 60px 0px 0px" : "0px",
+                  margin: isMax || isMax5 ? "-150px 60px 0px 0px" : "0px",
                 }}
                 onClick={(e: any) => handleSubmit(e)}
               >
